53665fc00c46a95b4f7cb12ca05c4b2a06a4f9ac
tameooo <remat01@mail.ru>
2021-05-02 12:14:14 +0600

    sorting added

--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -8,9 +8,13 @@ interface TableProps {
    data: IPlayer[]
    onDelete: (id: number) => void
    onItemClick: (player: IPlayer) => void
+   onAgeSort: () => void
+   onOvaSort: () => void
+   onPotSort: () => void
+   onValueSort: () => void
 }
 
-const Table: React.FC<TableProps> = ({ data, onDelete, onItemClick }) => {
+const Table: React.FC<TableProps> = ({ data, onDelete, onItemClick, onAgeSort, onOvaSort, onPotSort, onValueSort }) => {
    const getTableItems = (tableData: IPlayer[]): JSX.Element[] => {
       return tableData.map((item, idx) => {
          return (
@@ -28,11 +32,11 @@ const Table: React.FC<TableProps> = ({ data, onDelete, onItemClick }) => {
       <ul className="table">
          <li className="header">
             <div className="header-item">Name</div>
-            <div className="header-item">Age</div>
-            <div className="header-item">Ova</div>
-            <div className="header-item">Pot</div>
+            <div className="header-item hover" onClick={() => onAgeSort()}>Age</div>
+            <div className="header-item hover" onClick={() => onOvaSort()}>Ova</div>
+            <div className="header-item hover" onClick={() => onPotSort()}>Pot</div>
             <div className="header-item">Team Contract</div>
-            <div className="header-item">Value</div>
+            <div className="header-item hover" onClick={() => onValueSort()}>Value</div>
             <div className="header-item">Wage</div>
             <div className="header-item">Nationality</div>
             <div className="header-item">Joined</div>
